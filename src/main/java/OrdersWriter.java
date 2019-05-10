ae406f2a4e0a52a10b40ba09be91e8e50376ac43
unknown <batach31@hotmail.com>
2019-05-10 10:48:03 +0200

    refactor api | OrdersWriter & Product | toString implemented for products

--- src/main/java/OrdersWriter.java
+++ src/main/java/OrdersWriter.java
@@ -24,27 +24,7 @@ public class OrdersWriter {
             sb.append("\"products\": [");
             for (int j = 0; j < order.getProductsCount(); j++) {
                 Product product = order.getProduct(j);
-
-                sb.append("{");
-                sb.append("\"code\": \"");
-                sb.append(product.getCode());
-                sb.append("\", ");
-                sb.append("\"color\": \"");
-                sb.append(getColorFor(product));
-                sb.append("\", ");
-
-                if (!product.getSize().equals(Size.INVALID)) {
-                    sb.append("\"size\": \"");
-                    sb.append(getSizeFor(product));
-                    sb.append("\", ");
-                }
-
-                sb.append("\"price\": ");
-                sb.append(product.getPrice());
-                sb.append(", ");
-                sb.append("\"currency\": \"");
-                sb.append(product.getCurrency());
-                sb.append("\"}, ");
+                sb.append(product.toString());
             }
 
             if (order.getProductsCount() > 0) {
--- src/main/java/Product.java
+++ src/main/java/Product.java
@@ -41,4 +41,30 @@ public class Product {
     public Currency getCurrency() {
         return currency;
     }
+
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+        sb.append("{");
+        sb.append("\"code\": \"");
+        sb.append(code);
+        sb.append("\", ");
+        sb.append("\"color\": \"");
+        sb.append(color);
+        sb.append("\", ");
+
+        if (!size.equals(Size.INVALID)) {
+            sb.append("\"size\": \"");
+            sb.append(size);
+            sb.append("\", ");
+        }
+
+        sb.append("\"price\": ");
+        sb.append(price);
+        sb.append(", ");
+        sb.append("\"currency\": \"");
+        sb.append(currency);
+        sb.append("\"}, ");
+
+        return sb.toString();
+    }
 }
